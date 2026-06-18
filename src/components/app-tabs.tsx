d5b85d27d14993359b25fa41c9b35bf7d078afb7
devmasalati <devmasalati@gmail.com>
2026-06-18 16:48:35 +0000

    feat: add empty state for My Clips screen

--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -35,6 +35,14 @@ export default function AppTabs() {
           renderingMode="template"
         />
       </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="(tabs)/my-clips">
+        <NativeTabs.Trigger.Label>My Clips</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon
+          src={require('@/assets/images/tabIcons/explore.png')}
+          renderingMode="template"
+        />
+      </NativeTabs.Trigger>
     </NativeTabs>
   );
 }
